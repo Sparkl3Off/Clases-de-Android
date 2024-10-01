4500d568a8801a3e2be4ea5fb413d507b49fc370
Sparkl3Off <danielrebel1605@gmail.com>
2024-10-01 13:38:36 -0300

    Ingreso y despligue de datos

--- Ciclodevida/app/src/main/java/com/example/ciclodevida/MainActivity.java
+++ Ciclodevida/app/src/main/java/com/example/ciclodevida/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.ciclodevida;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+  private EditText dato1;
+  private TextView salida;
+  private TextView salida2;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     Toast.makeText(this, "Este es un Toast", Toast.LENGTH_SHORT).show();
+    dato1 = (EditText)findViewById(R.id.editTextNumber);
+    salida = (TextView)findViewById(R.id.textView2);
+    salida2 = (TextView)findViewById(R.id.textView3);
+  }
+
+  public void mostrar(View muestra) {
+    String datoSale = dato1.getText().toString();
+    salida.setText(datoSale);
+    String numero = dato1.getText().toString();
+    int valor = Integer.parseInt(numero);
+    valor = valor * 5;
+    String datoSale2 = String.valueOf(valor);
+    salida2.setText(datoSale2);
   }
 
   protected void onStart() {
